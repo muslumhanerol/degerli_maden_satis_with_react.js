d22162bde994d9d04eff5c0cb4b0baf308b0539b
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-06-21 23:10:37 +0300

    header_+

--- maden_satis/src/components/Header.jsx
+++ maden_satis/src/components/Header.jsx
@@ -3,10 +3,13 @@ import '../css/Header.css';
 
 function Header() {
   return (
-    <div>
+    <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+        <div className='flex-row'>
+            <img className="logo" src="./src/images/logo.png"/>
+            <p className="logo-text">DEGERLİ MADEN SATIŞ</p>
+        </div>
         <div>
-            <img className="logo" src="./src/images/logo.png"></img>
-            <p>Degerli Maden Satış</p>
+            <input className='search-input' type='text' placeholder='Ürünleri Ara'/>
         </div>
     </div>
   )
