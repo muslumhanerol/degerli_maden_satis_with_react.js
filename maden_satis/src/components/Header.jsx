8ccc1f47093f11fb364a0b5249f15ad1da977992
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-06-21 23:39:56 +0300

    header_fns

--- maden_satis/src/components/Header.jsx
+++ maden_satis/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/Header.css';
 import { SlBasket } from "react-icons/sl";
 import { CiLight } from "react-icons/ci";
@@ -6,18 +6,32 @@ import { FaRegMoon } from "react-icons/fa";
 
 
 function Header() {
+    const [mode , setmode] = useState(false);
+    const changeMode = () => {
+        const root = document.getElementById("root");
+        setmode(!mode);
+        if(mode){
+            root.style.backgroundColor="black";
+            root.style.color="#fff";
+        } else{
+            root.style.backgroundColor="#fff";
+            root.style.color="black";
+        }
+        setmode(!mode);
+
+    }
+
   return (
     <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
         <div className='flex-row'>
-            <img className="logo" src="./src/images/logo.png"/>
+            <img className="logo" src="./src/images/logo_bb.png"/>
             <p className="logo-text">DEGERLİ MADEN SATIŞ</p>
         </div>
         
         <div className='flex-row'>
             <input className='search-input' type='text' placeholder='Ürünleri Ara'/>
             <div className='icon'>
-            <CiLight />
-            {/* <FaRegMoon /> */}
+            {mode ? <FaRegMoon onClick={changeMode} /> : <CiLight onClick={changeMode} />}
             <SlBasket />
             </div>  
         </div>
