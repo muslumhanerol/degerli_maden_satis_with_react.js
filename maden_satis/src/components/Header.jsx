b6ab29cf1f55035b96880732fd8e11006675a002
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-06-21 22:54:15 +0300

    logo_ok

---
+++ maden_satis/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/Header.css';
+
+function Header() {
+  return (
+    <div>
+        <div>
+            <img className="logo" src="./src/images/logo.png"></img>
+            <p>Degerli Maden Satış</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
